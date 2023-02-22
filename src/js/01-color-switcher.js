00e268c6abcf9d1f5e22360348cf534c7c504b39
Volodymyr Derkach <bobikderkac@gmail.com>
2023-02-22 20:04:54 +0200

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,18 +3,17 @@ const stopBtn = document.querySelector('[data-stop]');
 
 startBtn.addEventListener('click', onUpdateBgColor);
 stopBtn.addEventListener('click', onStopUpdateBgColor);
-
+let intervalId = 0
 function onUpdateBgColor() {
-  return (id = setInterval(() => {
-    document.body.style.backgroundColor = `${getRandomHexColor()}`,
-
-    startBtn.disabled = true;
+  return (intervalId = setInterval(() => {
+    (document.body.style.backgroundColor = `${getRandomHexColor()}`),
+      (startBtn.disabled = true);
     stopBtn.disabled = false;
   }, 1000));
 }
 
 function onStopUpdateBgColor() {
-  clearInterval(id);
+  clearInterval(intervalId);
 
   startBtn.disabled = false;
   stopBtn.disabled = true;
