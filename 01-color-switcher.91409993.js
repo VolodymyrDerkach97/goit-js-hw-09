1d3aed8308e160c989f3b4a163b6e8a42ffaca10
VolodymyrDerkach97 <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-02-22 18:05:19 +0000

    Deploying to gh-pages from @ VolodymyrDerkach97/goit-js-hw-09@00e268c6abcf9d1f5e22360348cf534c7c504b39 🚀

---
+++ 01-color-switcher.91409993.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){return d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));let d=0;
+//# sourceMappingURL=01-color-switcher.91409993.js.map
